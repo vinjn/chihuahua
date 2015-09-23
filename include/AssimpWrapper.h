d4c8195c58ee3cf3000a6ec8eadca4f1bffe23e6
vinjn <vinjn.z@gmail.com>
2015-09-23 21:15:43 +0800

    Disable texture loading in Android mode

--- include/AssimpWrapper.h
+++
@@ -1,8 +0,0 @@
-#ifndef ASSIMP_WRAPPER_H
-#define ASSIMP_WRAPPER_H
-
-#include <irrlicht.h>
-
-IRRLICHT_API irr::scene::IAnimatedMesh* IRRCALLCONV getMeshFromAssimp(irr::scene::ISceneManager* smgr, const irr::io::path& path);
-
-#endif // ASSIMP_WRAPPER_H
